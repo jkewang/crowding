8628313180b87e2015eb151790c5d1774a51881d
jkewang <664875193@qq.com>
2019-03-08 13:17:51 +0800

    env_tfl

--- dqfd.py
+++ dqfd.py
@@ -9,6 +9,12 @@ class DQFD(object):
         self.LR = 1e-3
         self.GAMMA = 0.9
         self.BATCH_SIZE = 32
+        self.train_num = 0
+        self.replace_num = 300
+
+        self.filename = "data_dqn.txt"
+        self.f = open(self.filename)
+        self.MAX_LINE = 6250789
 
         self.s_list = []
         self.a_list = []
@@ -20,18 +26,63 @@ class DQFD(object):
         self.N_Action = 5
         self.tf_s = tf.placeholder(tf.float32, [None, self.N_State])
         self.tf_s_ = tf.placeholder(tf.float32, [None, self.N_State])
-        self.tf_a = tf.placeholder(tf.float32, [None,self.N_Action])
+        self.tf_a = tf.placeholder(tf.int32, [None,])
         self.tf_r = tf.placeholder(tf.float32, [None,])
 
         self.q = self.build_Q_net("Qnet", trainable=True)
         self.q_next = self.build_Qnext_net("Qnext", trainable=False)
+        self.input_q_values = tf.placeholder(tf.float32, [None], name='input_q_values')
+        a_indices = tf.stack([tf.range(tf.shape(self.tf_a)[0], dtype=tf.int32), self.tf_a], axis=1)
+        self.q_wrt_a = tf.gather_nd(params=self.q, indices=a_indices)
+
+        self.td_error = tf.reduce_mean(tf.squared_difference(self.input_q_values, self.q_wrt_a))
+        self.train_op = tf.train.AdamOptimizer(self.LR).minimize(self.td_error)
 
         self.sess = tf.Session()
         self.sess.run(tf.global_variables_initializer())
         self.saver = tf.train.Saver()
 
     def read_data(self):
+        pointer = 0
+        subdata = " "
+        while(pointer < self.MAX_LINE):
+            subdata += self.f.readline()
+            pointer += 1
 
+        singledata = str.split(subdata, '#')
+
+        for ii in range(len(singledata)-1):
+            state_i = []
+            state_i_ = []
+            tmp_end = singledata[ii].rfind("]", 0, 4000)
+            tmp_state = singledata[ii][2:tmp_end]
+            statespecific = tmp_state.split(" ")
+            for item in statespecific:
+                if item != '':
+                    state_i.append(float(item))
+
+            tmp_action = singledata[ii][tmp_end+2]
+
+            tmp_start2 = singledata[ii].rfind("[", 100, -1)
+            tmp_end2 = singledata[ii].rfind("]", 4000, -1)
+            tmp_state_ = singledata[ii][tmp_start2+1:tmp_end2]
+
+            statespecific2 = tmp_state_.split(" ")
+            for item in statespecific2:
+                if item != '':
+                    state_i_.append(float(item))
+
+            tmp_reward = singledata[ii][tmp_end2+2:-3]
+
+            tmp_done = singledata[ii][-1]
+
+            self.s_list.append(state_i)
+            self.snext_list.append(state_i_)
+            self.a_list.append(int(tmp_action))
+            self.r_list.append(float(tmp_reward))
+            self.done_list.append(int(tmp_done))
+
+        print(len(self.s_list),len(self.snext_list),len(self.a_list),len(self.r_list),len(self.done_list))
 
     def build_Q_net(self, name, trainable):
         with tf.variable_scope(name):
@@ -48,24 +99,34 @@ class DQFD(object):
             return l3
 
     def learn(self):
-        s_list, a_list, snext_list, r_list, done_list = self.read_batch()
-        q_eval = []
-        q_target = []
+        if self.train_num % self.replace_num == 0:
+            t_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Qnext')
+            e_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='Qnet')
+            self.sess.run([tf.assign(t, e) for t, e in zip(t_params, e_params)])
 
+
+        s_list, a_list, snext_list, r_list, done_list = self.read_batch()
+
+        self.q_target = []
+        self.q_eval = []
         a_indices = tf.stack([tf.range(tf.shape(self.tf_a)[0], dtype=tf.int32), self.tf_a], axis=1)
-        q_eval = tf.gather_nd(params=self.q, indices=a_indices)
+        self.q_eval = tf.gather_nd(params=self.q, indices=a_indices)
+        all_q_next = self.q_next.eval(session=self.sess, feed_dict={self.tf_s_: snext_list})
         for i in range(self.BATCH_SIZE):
             if done_list[i] == 0:
-                q_target.append(r_list[i] + self.GAMMA * np.max(self.q_next.eval(
-                    session=self.sess, feed_dict={self.tf_s_:snext_list})))
+                self.q_target.append(r_list[i] + self.GAMMA * np.max(all_q_next[i]))
             else:
-                q_target.append(r_list[i])
+                self.q_target.append(r_list[i])
 
-        td_error = tf.reduce_mean(tf.squared_difference(q_target, q_eval))
-        train_op = tf.train.AdamOptimizer(self.LR).minimize(td_error)
-        self.sess.run(train_op,{self.tf_s:s_list,
-            self.a_list:a_list, self.tf_s_:snext_list,self.tf_a:a_list})
+        self.sess.run(self.train_op,
+            {self.tf_s:s_list,self.tf_r:r_list, self.tf_s_:snext_list,
+             self.tf_a:a_list,self.input_q_values:self.q_target})
 
+        self.train_num += 1
+        if self.train_num % 100 == 0:
+            print(self.train_num,self.sess.run(self.td_error,
+                {self.tf_s:s_list,self.tf_r:r_list, self.tf_s_:snext_list,
+                 self.tf_a:a_list,self.input_q_values:self.q_target}))
 
     def read_batch(self):
         s_list = []
@@ -83,3 +144,15 @@ class DQFD(object):
             done_list.append(self.done_list[index])
 
         return s_list, a_list, snext_list, r_list, done_list
+
+    def save(self):
+        saver = tf.train.Saver()
+        saver.save(self.sess, './dqn_model/model.ckpt')
+        print("save done!")
+
+myDQFD = DQFD()
+myDQFD.read_data()
+for i in range(100000):
+    myDQFD.learn()
+    if i == 99999:
+        myDQFD.save()
---
+++ env_tfl.py
@@ -0,0 +1,293 @@
+import os,sys
+import change_trip
+
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'],'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable SUMO_HOME")
+
+config_path = "/home/jkwang/learn_sumo/straight/straight.sumo.cfg"
+sumoBinary = "/usr/bin/sumo"
+sumoguiBinary = "/usr/bin/sumo-gui"
+sumoCmd = [sumoBinary,"-c",config_path,"--collision.action","remove","--start","--no-step-log","--no-warnings","--no-duration-log"]
+
+import traci
+import traci.constants as tc
+import math
+import numpy as np
+
+DEAD_LINE = 300
+
+class TrafficEnv(object):
+
+    def __init__(self):
+        traci.start(sumoCmd)
+
+        #Env --lanechange.duration
+        self.step_num = 0
+        self.AgentId = "agent"
+        self.VehicleIds = []
+        self.TotalReward = 0
+        self.StartTime = 0
+        self.end = 0
+        #traci.vehicle.add("agent", "agent_route")
+        #traci.gui.trackVehicle('View #0', "agent")
+
+        #States
+        self.OccMapState = np.zeros((40, 7))
+        self.VehicleState = [0,0,0]
+        self.RoadState = [0 for i in range(9)]
+        self.state = None
+        self.LeaderList = set()
+
+        #property to simulate
+        self.end_x = 0
+        self.end_y = 2000
+        self.AgentX = 0
+        self.AgentY = 0
+        self.AgentSpeed = 10
+        self.AgentAccRate = 100
+        self.AgentDecRate = 300
+        self.minLaneNumber = 0
+        self.maxLaneNumber = 1
+        self.oldDistance = 0
+        self.nowDistance = 0
+        self.steptime = 0
+
+    def reset(self):
+        try:
+            self.end = 0
+            self.TotalReward = 0
+            self.oldDistance = 0
+            self.nowDistance = 0
+            self.lastdistance = 0.99
+            self.x_v = 0
+            self.y_v = 0
+            self.is_in = 0
+            self.steptime = 0
+            self.LeaderList = set()
+
+            change_trip.change()
+            traci.load(["-c",config_path,"--collision.action","remove","--no-step-log","--no-warnings","--no-duration-log"])
+
+            print("Resetting...")
+            #traci.vehicle.add("agent", "agent_route")
+            #traci.gui.trackVehicle('View #0', "agent")
+
+            self.OccMapState = np.zeros((40, 7))
+            rawOcc = self.OccMapState
+
+            traci.simulationStep()
+            AgentAvailable = False
+            while AgentAvailable == False:
+                traci.simulationStep()
+                self.VehicleIds = traci.vehicle.getIDList()
+                if self.AgentId in self.VehicleIds:
+                    AgentAvailable = True
+                    self.StartTime = traci.simulation.getCurrentTime()
+            for vehId in self.VehicleIds:
+                traci.vehicle.subscribe(vehId,(tc.VAR_SPEED,tc.VAR_POSITION,tc.VAR_LANE_INDEX,tc.VAR_DISTANCE))
+                traci.vehicle.subscribeLeader(self.AgentId,50)
+                if vehId == self.AgentId:
+                    traci.vehicle.setSpeedMode(self.AgentId,0)
+                    traci.vehicle.setLaneChangeMode(self.AgentId,0)
+            self.state,breakstop,overtake,rawOcc= self.perception()
+        except:
+            traci.start(sumoCmd)
+            print("retrying")
+            self.reset()
+        return self.state, rawOcc
+
+    def step(self,action):
+        # define action:
+        # action  |     meaning
+        #    0    |    go straight
+        #    1    |    break down
+        #    2    |    change left
+        #    3    |    change right
+        #    4    |    do nothing
+        rawOcc = self.OccMapState
+
+        for vehId in self.VehicleIds:
+            traci.vehicle.subscribe(vehId,(tc.VAR_SPEED,tc.VAR_POSITION,tc.VAR_LANE_INDEX,tc.VAR_DISTANCE))
+            traci.vehicle.subscribeLeader(self.AgentId,50)
+            if vehId == self.AgentId:
+                traci.vehicle.setSpeedMode(self.AgentId,0)
+                traci.vehicle.setLaneChangeMode(self.AgentId,0)
+                self.is_in = 1
+                #traci.gui.trackVehicle('View #0', "agent")
+
+        self.maxLaneNumber = 2
+
+        self.end = 0
+        reward = 0
+        DistanceTravelled = 0
+        if self.is_in == 1:
+            if action == 0:
+                maxSpeed = 30
+                time = (maxSpeed - (traci.vehicle.getSubscriptionResults(self.AgentId)[tc.VAR_SPEED])) / self.AgentAccRate
+                traci.vehicle.slowDown(self.AgentId, maxSpeed, 100*time)
+            elif action == 1:
+                time = ((traci.vehicle.getSubscriptionResults(self.AgentId)[tc.VAR_SPEED]) - 0)/self.AgentDecRate
+                traci.vehicle.slowDown(self.AgentId, 0, 100*time)
+            elif action == 2:
+                laneindex = traci.vehicle.getSubscriptionResults(self.AgentId)[tc.VAR_LANE_INDEX]
+                if laneindex < self.maxLaneNumber:
+                    traci.vehicle.changeLane(self.AgentId,laneindex+1,100)
+                traci.vehicle.setSpeed(self.AgentId, traci.vehicle.getSubscriptionResults(self.AgentId)[tc.VAR_SPEED])
+            elif action == 3:
+                laneindex = traci.vehicle.getSubscriptionResults(self.AgentId)[tc.VAR_LANE_INDEX]
+                if laneindex > self.minLaneNumber:
+                    traci.vehicle.changeLane(self.AgentId,laneindex-1,100)
+                traci.vehicle.setSpeed(self.AgentId, traci.vehicle.getSubscriptionResults(self.AgentId)[tc.VAR_SPEED])
+            elif action == 4:
+                traci.vehicle.setSpeed(self.AgentId,traci.vehicle.getSubscriptionResults(self.AgentId)[tc.VAR_SPEED])
+
+        traci.simulationStep()
+        self.steptime += 1
+        self.VehicleIds = traci.vehicle.getIDList()
+
+        if self.is_in == 1:
+            if self.AgentId in self.VehicleIds and self.steptime < DEAD_LINE:
+                for vehId in self.VehicleIds:
+                    traci.vehicle.subscribe(vehId,(tc.VAR_SPEED,tc.VAR_POSITION,tc.VAR_LANE_INDEX,tc.VAR_DISTANCE))
+                    traci.vehicle.subscribeLeader(self.AgentId,50)
+
+                Vehicle_Params = traci.vehicle.getSubscriptionResults(self.AgentId)
+                self.AutocarSpeed = Vehicle_Params[tc.VAR_SPEED]
+                posAutox = Vehicle_Params[tc.VAR_POSITION]
+                if math.sqrt((self.end_x-posAutox[0])**2+(self.end_y-posAutox[1])**2)<30:
+                    self.end = 100
+
+                self.state,breakstop,overtake,rawOcc = self.perception()
+                reward = self.cal_reward(self.end,breakstop,overtake)
+            elif self.steptime < DEAD_LINE:
+                #self.state = self.perception()
+                self.end = 1
+                reward = self.cal_reward(is_collision=self.end,breakstop=0,overtake=0)
+                DistanceTravelled = 0
+            else:
+                self.end = 1
+                reward = self.cal_reward(is_collision=2, breakstop=0,overtake=0)
+                DistanceTravelled = 0
+
+        return self.state, reward, self.end, DistanceTravelled, rawOcc
+
+    def cal_reward(self,is_collision,breakstop,overtake):
+        if is_collision == 1:
+            print("collision!")
+            return -100
+        elif is_collision == 2:
+            print("overtime")
+            return -100
+        elif is_collision == 100:
+            print("arrive!")
+            return 100
+        else:
+            self.nowDistance = traci.vehicle.getDistance(self.AgentId)
+            del_distance = self.nowDistance - self.oldDistance
+            reward = float(del_distance-8)/4 * self.nowDistance/500 + 5*overtake
+
+            self.oldDistance = self.nowDistance
+            if breakstop == 1:
+                reward -= 5
+
+            return reward
+
+    def perception(self):
+
+        #the state is defined as:
+        # 0   | 1    | 2    | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 |
+        #speed|cos(a)|sin(a)|l? |r? |dis| r | y | g |l? | c? | r? |
+        #
+
+        self.VehicleIds = traci.vehicle.getIDList()
+
+        AllVehicleParams = []
+
+        #----------------------------to get the vehicle state------------------------
+        for vehId in self.VehicleIds:
+            traci.vehicle.subscribe(vehId, (tc.VAR_SPEED, tc.VAR_POSITION, tc.VAR_ANGLE, tc.VAR_LANE_INDEX, tc.VAR_DISTANCE, tc.VAR_LANE_ID))
+            VehicleParam = traci.vehicle.getSubscriptionResults(vehId)
+            #AllVehicleParams.append(vehId)
+            if vehId != self.AgentId:
+                AllVehicleParams.append(VehicleParam)
+            else:
+                self.AgentSpeed = VehicleParam[tc.VAR_SPEED]
+                self.AgentAngle = (VehicleParam[tc.VAR_ANGLE]/180)*math.pi
+                self.AgentX = VehicleParam[tc.VAR_POSITION][0]
+                self.AgentY = VehicleParam[tc.VAR_POSITION][1]
+        self.VehicleState = [self.AgentSpeed/20,math.cos(self.AgentAngle),math.sin(self.AgentAngle)]
+
+        #---------------------to calculate the occupanied state-----------------------
+        LOW_X_BOUND = -6
+        HIGH_X_BOUND = 6
+        LOW_Y_BOUND = -18
+        HIGH_Y_BOUND = 60
+        self.OccMapState = np.zeros((40, 7))
+        raw_OccMapState = self.OccMapState
+
+        for VehicleParam in AllVehicleParams:
+            VehiclePos = VehicleParam[tc.VAR_POSITION]
+            rol = math.sqrt((VehiclePos[0]-self.AgentX)**2+(VehiclePos[1]-self.AgentY)**2)
+            theta = math.atan2(VehiclePos[1]-self.AgentY,VehiclePos[0]-self.AgentX)
+
+            reltheta = theta + self.AgentAngle
+            relX = rol*math.cos(reltheta)
+            relY = rol*math.sin(reltheta)
+            if (relX>LOW_X_BOUND and relX<HIGH_X_BOUND) and (relY>LOW_Y_BOUND and relY<HIGH_Y_BOUND):
+                indexX = int((6 + relX)/2 + 0.5)
+                indexY = int((60 - relY)/2 + 0.5)
+
+                self.OccMapState[indexY,indexX] = 1.0
+                raw_OccMapState = self.OccMapState
+            #add for fc dqn
+        self.OccMapState = self.OccMapState.reshape(-1)
+
+        #-------------------------------to get the RoadState----------------------------
+        #RoadState: [leftcan rightcan distance r y g leftava centerava rightava]
+        self.RoadState = [1.0 for i in range(9)]
+        now_laneindex = 0
+
+        overtake = 0
+        remove_list = []
+        for vehId in self.VehicleIds:
+            if vehId == self.AgentId:
+                now_laneindex = traci.vehicle.getSubscriptionResults(self.AgentId)[tc.VAR_LANE_INDEX]
+                leaderinfo = traci.vehicle.getLeader(self.AgentId)
+                if leaderinfo != None:
+                    leader = leaderinfo[0]
+                    self.LeaderList.add(leader)
+                    myposition = traci.vehicle.getSubscriptionResults(self.AgentId)[tc.VAR_POSITION][1]
+                    for vehicle in self.LeaderList:
+                        #print(vehicle)
+                        try:
+                            position = traci.vehicle.getSubscriptionResults(vehicle)[tc.VAR_POSITION][1]
+                            if myposition > position:
+                                print("overtaking!")
+                                overtake = 1
+                                remove_list.append(vehicle)
+                        except:
+                            pass
+                            #print("position get failed")
+                    for loser in remove_list:
+                        self.LeaderList.remove(loser)
+
+        if now_laneindex == 0:
+            self.RoadState = [0,1,1.000,0,0,1,0,1,1]
+        elif now_laneindex == 2:
+            self.RoadState = [1,0,1.000,0,0,1,1,1,0]
+        else:
+            self.RoadState = [0, 0, 1.000, 0, 0, 1, 1, 1, 1]
+
+
+        if self.AgentSpeed <= 1:
+            breakstop = 1
+            print("breakstop")
+        else:
+            breakstop = 0
+
+
+        return [self.OccMapState,self.VehicleState,self.RoadState],breakstop,overtake,raw_OccMapState
+
